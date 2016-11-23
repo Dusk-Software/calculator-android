877fcc8a9ca46ab64a30a4fb40f1bdca8a379893
YannisLewis <yannisdl@hotmail.com>
2016-11-23 11:53:35 -0500

    added NumericButtonOnClickListener

--- app/src/main/java/com/dusk/calculator/MainActivity.java
+++ app/src/main/java/com/dusk/calculator/MainActivity.java
@@ -2,12 +2,29 @@ package com.dusk.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView numericInput;
+    Button pressedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numericInput = (TextView) findViewById(R.id.textInput);
     }
+
+    public void NumericButtonOnClickListener(View v) {
+        pressedButton = (Button) findViewById(v.getId());
+        numericInput.append(pressedButton.getText());
+    }
+
+
 }
+
